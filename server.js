69bf441ab9fad1a0748feef12c979a4a406d92a2
Rodrigo Urrutia Gonz <rodrigo.urrutia@rutiandco.com>
2024-07-24 18:58:45 -0400

    try enabling files in public folder

--- server.js
+++ server.js
@@ -1,16 +1,51 @@
-const { createServer } = require("http");
-const next = require("next");
+const NextServer = require('next/dist/server/next-server').default
+const fs = require('fs')
+const http = require("http")
+const path = require("path")
+const url = require("url")
+const nextConfig = require('./next.config.json');
+const { NextServer } = require("next/dist/server/next");
 
-const port = process.env.PORT || 8080;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const getPort = process.env.PORT || 8080
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    handle(req, res);
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
-});
+const getEnv = process.env.NODE_ENV
+
+let nextRequestHandler
+
+const server = http.createServer(async (req, res) => {
+    const parsedUrl = url.parse(req.url, true)
+    try {
+        if(parsedUrl.pathname.startsWith('/public/')) {
+            const filePath = path.join(__dirname, parsedUrl.pathname)
+            res.writeHead(200, { 'Content-Type': 'image/svg+xml' })
+            return fs.createReadStream(filePath).pipe(res)
+        }
+        await nextRequestHandler(req, res, parsedUrl)
+    } catch (err) {
+        console.error(err)
+        res.statusCode=500
+        res.end('Internal server error')
+    }
+})
+
+const serverEnv = getEnv
+
+const serverConfig = {
+    hostname: '0.0.0.0',
+    port: getPort,
+    dir: path.join(__dirname),
+    dev: serverEnv === 'development',
+    conf: nextConfig
+}
+
+server.listen(serverConfig.port, serverConfig.hostname, (err) => {
+    if (err) {
+        console.error('Failed to start server', err)
+        process.exit(1)
+    }
+    const nextServer = new NextServer(serverConfig)
+    nextRequestHandler = nextServer.getRequestHandler()
+    console.log(
+        `> Server listening at http://${serverConfig.hostname}:${serverConfig.port} as ${serverEnv} env`
+    )
+})
