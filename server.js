927aaae7d36b912591556eac41c186de5962a036
Rodrigo Urrutia Gonz <rodrigo.urrutia@rutiandco.com>
2024-07-24 23:40:48 -0400

    reconfig for public files

--- server.js
+++ server.js
@@ -1,62 +1,50 @@
-const fs = require('fs')
-const NextServer = require('next/dist/server/next-server').default
-const http = require("http")
-const path = require("path")
-const url = require("url")
-const nextConfig = require('./next.config.json');
-const { NextServer } = require("next/dist/server/next");
+const { createServer } = require('http');
+const { parse } = require('url');
+const fs = require('fs');
+const path = require('path');
+const next = require('next');
+const { create } = require('domain')
 
-const getPort = process.env.PORT || 8080
+const stage = process.env.NODE_ENV !== 'production';
+const hostname = '0.0.0.0';
+const port = process.env.PORT || 8080;
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
 
-const getEnv = process.env.NODE_ENV
+const mimeTypes = {
+    '.svg': 'image/svg+xml',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.gif': 'image/gif',
+    // Agrega más tipos de contenido si es necesario
+};
 
-let nextRequestHandler
-
-const server = http.createServer(async (req, res) => {
-    const parsedUrl = url.parse(req.url, true)
-    const filePath = path.join(__dirname, 'public', parsedUrl.pathname)
-    try {
-        if(parsedUrl.pathname.startsWith('/')) {
-            if (fs.existsSync(filePath)) {
-                const mimeTypes = {
-                    '.svg': 'image/svg+xml',
-                    '.png': 'image/png',
-                    '.jpg': 'image/jpeg',
-                    '.gif': 'image/gif',
-                    // Agrega más tipos de contenido si es necesario
-                };
-                const ext = path.extname(filePath)
-                const contentType = mimeTypes[ext] || 'application/octet-stream';
-                res.writeHead(200, { 'Content-Type': contentType })
-                return fs.createReadStream(filePath).pipe(res)
+app.prepare().then(() => {
+    createServer(async(req, res) => {
+        try {
+            const parsedUrl = parse(req.url, true);
+            const { pathname } = parsedUrl;
+            if (pathname.startsWith('/')) {
+                const filePath = path.join(__dirname, 'public', pathname);
+                if (fs.existsSync(filePath)) {
+                    const ext = path.extname(filePath);
+                    const contentType = mimeTypes[ext] || 'application/octet-stream';
+                    res.writeHead(200, { 'Content-Type': contentType });
+                    return fs.createReadStream(filePath).pipe(res);
+                }
             }
+            await handle(req, res, parsedUrl);
+        } catch (err) {
+            console.error('Error occurred handling', req.url, err);
+            res.statusCode = 500;
+            res.end('Internal server error');
         }
-        await nextRequestHandler(req, res, parsedUrl)
-    } catch (err) {
-        console.error(err)
-        res.statusCode=500
-        res.end('Internal server error')
-    }
-})
-
-const serverEnv = getEnv
-
-const serverConfig = {
-    hostname: '0.0.0.0',
-    port: getPort,
-    dir: path.join(__dirname),
-    dev: serverEnv === 'development',
-    conf: nextConfig
-}
-
-server.listen(serverConfig.port, serverConfig.hostname, (err) => {
-    if (err) {
-        console.error('Failed to start server', err)
-        process.exit(1)
-    }
-    const nextServer = new NextServer(serverConfig)
-    nextRequestHandler = nextServer.getRequestHandler()
-    console.log(
-        `> Server listening at http://${serverConfig.hostname}:${serverConfig.port} as ${serverEnv} env`
-    )
-})
+    })
+        .once('error', (err) => {
+            console.error(err);
+            process.exit(1);
+        })
+        .listen(port, () => {
+            console.log(`> Ready on http://${hostname}:${port}`);
+        });
+});
